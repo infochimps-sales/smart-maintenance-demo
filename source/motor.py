d2ead71e2fc44640d9a063388000b304172184e1
JoeHahnIC <joe.hahn@infochimps.com>
2015-05-21 17:03:15 -0500

    better

--- source/motor.py
+++ source/motor.py
@@ -6,23 +6,26 @@ from sklearn.svm import SVC
 
 class Motor:
 
-    def __init__(self, id, time, maint_type, fail_prob_rate, time_previous_maint, maint_interval, 
-            maint_duration, repair_duration):
+    def __init__(self, id, maint_type, fail_prob_rate, maint_interval, maint_duration, 
+            repair_duration):
         self.id = id
         self.maint_type = maint_type
         self.fail_prob_rate = fail_prob_rate
-        self.time_previous_maint = time_previous_maint
+        self.time_previous_maint = 0.0
         self.maint_interval = maint_interval
         self.maint_duration = maint_duration
         self.repair_duration = repair_duration
         self.time_when_repaired = None
+        self.just_failed = False
         self.time_next_maint = self.time_previous_maint + self.maint_interval
         self.state = 'operating'
         self.clf = SVC(kernel='poly', degree=3)
+        self.events = pd.DataFrame()
         
     def status(self, time):
         return {'time':time, 'id':self.id, 'state':self.state, 'maint_type': self.maint_type,
-            'fail_prob':self.fail_prob(time)}
+            'time_previous_maint':self.time_previous_maint, 'fail_prob':self.fail_prob(time),
+            'just_failed':self.just_failed}
 
     def fail_prob(self, time):
         return self.fail_prob_rate*(time - self.time_previous_maint)
@@ -32,27 +35,38 @@ class Motor:
         rn = np.random.uniform(low=0.0, high=1.0, size=None)
         if (rn < failprob):
             self.state = 'failed'
+            self.just_failed = True
             self.time_when_repaired = time + self.repair_duration
 
     def maint_check(self, time):
-        if ((self.state == 'operating') and (time >= self.time_next_maint)):
-            self.state = 'maintenance'
-            self.time_when_repaired = time + self.maint_duration
         if ((self.state == 'maintenance') and (time >= self.time_when_repaired)):
+            #motor transitions from maintenance to operating
             self.state = 'operating'
             self.time_previous_maint = time 
             self.time_next_maint = time + self.maint_interval
-        
+        if (self.maint_type == 'scheduled'):
+            #scheduled maintenance is begun
+            if ((self.state == 'operating') and (time >= self.time_next_maint)):
+                self.state = 'maintenance'
+                self.time_when_repaired = time + self.maint_duration
+        if (self.maint_type == 'run-to-fail'):
+            pass
+ 
     def repair_check(self, time):
         if (time >= self.time_when_repaired):
             self.state = 'operating'
             self.time_previous_maint = time 
             self.time_next_maint = time + self.maint_interval
+        else:
+            self.just_failed = False
 
     def operate(self, time):
+        if (self.state == 'failed'): 
+            self.repair_check(time) 
         if (self.state == 'operating'): 
             self.failure_check(time)
             self.maint_check(time)
         if (self.state == 'maintenance'): 
             self.maint_check(time)
-        if (self.state == 'failed'): self.repair_check(time)
+        dict = self.status(time)
+        self.events = self.events.append([dict], ignore_index=True)
--- source/smart_maint.py
+++ source/smart_maint.py
@@ -1,32 +1,70 @@
-#sm.py
+#smart_maint.py
 
 #imports
 import numpy as np
 import pandas as pd
 from motor import Motor
 
-#parameters
-time_final = 100
+#parameters for scheduled maintenance
 fail_prob_rate = 0.01
-time_previous_maint = 0
 maint_interval = 10
 maint_duration = 2
 repair_duration = 5
 
 #
-rn_seed = 1
+rn_seed = 13
 np.random.seed(rn_seed)
-motor_id = 1001
-time_init = 0
-events = pd.DataFrame()
-maint_mode = 'scheduled'
 
 #create motor
-m = Motor(motor_id, time_init, maint_type, fail_prob_rate, time_previous_maint, maint_interval, 
-    maint_duration, repair_duration)
+maint_type = 'run-to-fail'
+time_previous_maint = 0
+motor_id = 1001
+m = Motor(motor_id, maint_type, fail_prob_rate, maint_interval, maint_duration, repair_duration)
 
-#
-for t in np.arange(time_init, time_final, 1):
+#run motor using scheduled maintenance
+time_final = 2000
+for t in np.arange(0, time_final, 1):
     m.operate(t)
-    events = events.append([m.status(t)], ignore_index=True)
-    print m.status(t) 
+
+#widen display for dataframes
+pd.set_option('display.expand_frame_repr', False)
+
+#run motor using predictive maintenance
+m.maint_type = 'predictive'
+
+#generate training data
+events = m.events.copy(deep=True)
+idx = events[ (events.state == 'failed') & (events.just_failed == True) ].index.max()
+events = events[0:idx + 1]
+events['time_to_fail'] = float(np.nan)
+events.loc[events.state != 'operating', 'time_to_fail'] = -1
+events_shift = events.shift(periods=-1)
+while(events.time_to_fail.isnull().sum() > 0):
+    idx = (events.state == 'operating') & (events_shift.just_failed == True)
+    events.loc[idx, 'time_to_fail' ] = events_shift[idx].time - events[idx].time
+    events_shift = events_shift.shift(periods=-1)
+
+events = events[events.state == 'operating'].sort(columns='fail_prob')
+x = events.fail_prob.values
+x = x.reshape((len(x),1))
+x_avg = x.mean()
+x_std = x.std() 
+x_train = (x - x_avg)/x_std
+y_train = events.time_to_fail.values.astype(int)
+from sklearn.svm import SVC
+clf = SVC(kernel='poly', degree=3)
+clf.fit(x_train, y_train)
+print 'accuracy of SVM fit to training data = ', clf.score(x_train, y_train)
+
+import matplotlib.pyplot as plt
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+ax.set_xlabel('fail_prob')
+ax.set_ylabel('Time to fail')
+ax.set_title('poly degree=3')
+x_train_denormalized = x_train*x_std + x_avg
+ax.plot(x_train_denormalized, y_train, marker='o', linestyle='None')
+y_predict = clf.predict(x_train)
+ax.plot(x_train_denormalized, y_predict)
+plt.show(block=False)
+
