8898ac12e0a5055a75ff11f04a5bb72a0f069b14
JoeHahnIC <joe.hahn@infochimps.com>
2015-06-01 09:58:57 -0500

    multiple motors

--- source/smart_maint.py
+++ source/smart_maint.py
@@ -13,7 +13,7 @@ maint_duration = 2
 smart_maint_threshold = 1
 repair_duration = 3
 N_motors = 100
-run_interval = 500
+run_interval = 250
 ran_num_seed = 13
 
 #set random number seed
@@ -46,84 +46,54 @@ for t in np.arange(Time_init, Time_final):
 #train
 pd.set_option('display.expand_frame_repr', False)
 events_df = pd.DataFrame()
-for m in motors:
-    events_df = events_df.append(pd.DataFrame(m.events))
+for m in motors: events_df = events_df.append(pd.DataFrame(m.events))
 events_sched = events_df[(events_df.maint_type == 'scheduled')]
-events_sched.loc[events_sched.state == 'maintenance', 'state'] = 'operating'
+events_sched = events_df[(events_df.maint_type == 'scheduled') &
+    (events_df.state != 'maintenance')]
 events_sub = events_sched[['id', 'fail_prob', 'state']]
 N = events_sub.groupby(['fail_prob', 'state']).count().unstack()['id'].reset_index()
-N[N.isnull()] = 0.5
-N['percent_failed'] = np.round(N.repair*100.0/(N.operating + N.repair))
+N[N.isnull()] = 0.0
+N['total'] = N.operating + N.repair
+N['percent_failed'] = np.round(N.repair*100.0/N.total)
+N['weight'] = N.total**(0.5)
 print N
 x = N.fail_prob.values
-x = x.reshape((len(x), 1))
-x_avg = x.mean()
-x_std = x.std() 
-x_train = (x - x_avg)/x_std
+x_train = x.reshape((len(x), 1))
+x_train_avg = x_train.mean()
+x_train_std = x_train.std()
+x_train_norm = (x_train - x_train_avg)/x_train_std
 y_train = N.percent_failed.values.astype(int)
-clf = SVC(kernel='poly', degree=3)
-clf.fit(x_train, y_train)
-
-
-
-#store events in this file
-file = open('sm_events.json','w')
-for m in motors:
-    for d in m.events:
-        file.write(str(d) + '\n')
-        print d
-file.close()
-
-
-
-#
-import sys
-sys.exit()
-
-
-
-#
-import sys
-sys.exit()
-
-
-#run motor using scheduled maintenance
-m.maint_type = 'scheduled'
-print m.maint_type
-Time_init = Time_final
-Time_final += run_interval
-for t in np.arange(Time_init, Time_final):
-    m.operate(t)
-
-#run motor using predictive maintenance
-m.maint_type = 'predictive'
-print m.maint_type
-x_train, y_train = m.train()
-Time_init = Time_final
-Time_final += run_interval
-for t in np.arange(Time_init, Time_final):
-    m.operate(t)
-
-##show all events
-#for d in m.events:
-#    print d
-
-#repair stats
-pd.set_option('display.expand_frame_repr', False)
-events_df = pd.DataFrame(m.events)
-r = events_df.groupby(['maint_type', 'state']).count().Time
-print r
+weight = N.weight.values
+clf = SVC(kernel='rbf')
+clf.fit(x_train_norm, y_train, sample_weight=weight)
+print 'accuracy of SVM training = ', clf.score(x_train_norm, y_train)
 
 #plot time-to-fail vs fail-probability
 import matplotlib.pyplot as plt
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 ax.set_xlabel('fail_prob')
-ax.set_ylabel('Time to fail')
-ax.set_title('poly degree=3')
-ax.plot(x_train, y_train, marker='o', linestyle='None', markersize=2)
-x_train_norm = (x_train - m.x_avg)/m.x_std
-y_predict = m.clf.predict(x_train_norm)
-ax.plot(x_train, y_predict)
+ax.set_ylabel('% failures')
+ax.set_title('SVM prediction')
+#ax.plot(x_train, y_train, marker='o', linestyle='None', markersize=5)
+ax.scatter(x_train, y_train, s=weight)
+y_train_predicted = clf.predict(x_train_norm)
+ax.plot(x_train, y_train_predicted)
 plt.show(block=False)
+
+#store events in this file
+file = open('sm_events.json','w')
+for m in motors:
+    for d in m.events:
+        file.write(str(d) + '\n')
+        #print d
+        
+file.close()
+
+
+##repair stats
+#pd.set_option('display.expand_frame_repr', False)
+#events_df = pd.DataFrame(m.events)
+#r = events_df.groupby(['maint_type', 'state']).count().Time
+#print r
 
