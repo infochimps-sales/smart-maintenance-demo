a0a0ed6e7e3e9ee8e7602388ae3e1698b1d6bcd8
JoeHahnIC <joe.hahn@infochimps.com>
2015-06-23 16:33:21 -0500

    parallelized

--- sm_spark.py
+++ sm_spark.py
@@ -23,7 +23,7 @@ from pylab import *
 #setup for spark
 from pyspark import SparkConf, SparkContext
 conf = SparkConf().setMaster("yarn-client").setAppName("Smart Maintenance").set(
-    "spark.executor.memory", "10g")
+    "spark.executor.memory", "7g")
 sc = SparkContext(conf=conf, pyFiles=['helper_functions.py', 'motor.py'])
 
 
