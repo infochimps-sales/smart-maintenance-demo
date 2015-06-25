0ee06527738a799403a8b44029bb472ce938581a
JoeHahnIC <joe.hahn@infochimps.com>
2015-06-25 15:38:55 -0500

    installer

--- install.sh
+++ install.sh
@@ -9,13 +9,11 @@ hosts=( 'cdh-foyer' 'cdh-nn' 'cdh-rm' 'cdh-hh' )
 for host in "${hosts[@]}"; do
     ssh -A $host 'echo; echo; echo installing python libraries on $(hostname)...;
         echo; echo;
-        rm -rf /data/Anaconda-2.1.0-Linux-x86_64.sh*;
-        URL=http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/Anaconda-2.1.0-Linux-x86_64.sh;
-        wget -nv $URL --output-document=/data/Anaconda-2.1.0-Linux-x86_64.sh;
-        chmod +x /data/Anaconda-2.1.0-Linux-x86_64.sh;
-        rm -rf /data/anaconda;
-        /data/Anaconda-2.1.0-Linux-x86_64.sh -b -p /data/anaconda;
-        rm -rf /data/Anaconda-2.1.0-Linux-x86_64.sh;
+        rm -rf Anaconda-2.1.0-Linux-x86_64.sh* anaconda;
+        wget -nv http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/Anaconda-2.1.0-Linux-x86_64.sh;
+        chmod +x Anaconda-2.1.0-Linux-x86_64.sh;
+        ./Anaconda-2.1.0-Linux-x86_64.sh -b;
+        rm -rf Anaconda-2.1.0-Linux-x86_64.sh;
         rm -rf /home/$USER/anaconda;
         rm -rf /home/$USER/Anaconda-2.1.0-Linux-x86_64.sh*'
 done
