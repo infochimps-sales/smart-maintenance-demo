a03f08a16f42c860a279503e21f5423e2c4f5d49
JoeHahnIC <joe.hahn@infochimps.com>
2015-06-25 15:13:48 -0500

    readme

--- install.sh
+++ install.sh
@@ -4,16 +4,20 @@
 #
 #To execute:    ./install
 
-#install anaconda python on all hadoop nodes
+#install anaconda python on the /data drive on all hadoop nodes
 hosts=( 'cdh-foyer' 'cdh-nn' 'cdh-rm' 'cdh-hh' )
+URL=http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/Anaconda-2.1.0-Linux-x86_64.sh
 for host in "${hosts[@]}"; do
     ssh -A $host 'echo; echo; echo installing python libraries on $(hostname)...;
         echo; echo;
-        rm -rf home/$USER/anaconda;
-        wget -nv http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/Anaconda-2.1.0-Linux-x86_64.sh;
-        chmod +x Anaconda-2.1.0-Linux-x86_64.sh;
+        rm -rf /data/Anaconda-2.1.0-Linux-x86_64.sh*;
+        wget -nv $URL --output-document=/data/Anaconda-2.1.0-Linux-x86_64.sh;
+        chmod +x /data/Anaconda-2.1.0-Linux-x86_64.sh;
+        rm -rf /data/anaconda;
+        /data/Anaconda-2.1.0-Linux-x86_64.sh -b -p /data/anaconda;
+        rm -rf /data/Anaconda-2.1.0-Linux-x86_64.sh;
         rm -rf /home/$USER/anaconda;
-        ./Anaconda-2.1.0-Linux-x86_64.sh -b'
+        rm -rf /home/$USER/Anaconda-2.1.0-Linux-x86_64.sh*'
 done
 
 #start webserver in background, to browse output
