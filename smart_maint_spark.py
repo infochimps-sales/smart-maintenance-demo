e98f61589241b4f656a167df0466e3759b285452
JoeHahnIC <joe.hahn@infochimps.com>
2015-06-25 19:54:46 -0500

    INFO

--- smart_maint_spark.py
+++ smart_maint_spark.py
@@ -119,6 +119,7 @@ for t in np.arange(Time_start_pred_maint, Time_stop_pred_maint):
     motors = motors.map(lambda m: m.operate())
     #this inelegant step triggers lazy execution and avoids 'excessively deep recursion' error
     #if (t%100 == 99): motors = motors.sortBy(lambda m: m.id)
+    if (t%100 == 99): motors.persist()
 
 motors.persist()
 
