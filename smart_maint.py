dfc39a3576d45eb0556e9871c63d3a5e964a5b19
JoeHahnIC <joe.hahn@infochimps.com>
2015-06-26 14:38:40 -0500

    spark

--- smart_maint.py
+++ smart_maint.py
@@ -91,7 +91,7 @@ print 'maintenance mode:', motors.first().maint_type
 for t in np.arange(Time_start_runtofail, Time_stop_runtofail):
     motors = motors.map(lambda m: m.operate())
     #this inelegant step triggers lazy execution and avoids 'excessively deep recursion' error
-    if (t%300 == 299): motors = motors.sortBy(lambda m: m.id)
+    if (t%10 == 9): motors = motors.sortBy(lambda m: m.id)
 
 motors.persist()
 
@@ -102,7 +102,7 @@ print 'maintenance mode:', motors.first().maint_type
 for t in np.arange(Time_start_sched_maint, Time_stop_sched_maint):
     motors = motors.map(lambda m: m.operate())
     #this inelegant step triggers lazy execution and avoids 'excessively deep recursion' error
-    if (t%300 == 299): motors = motors.sortBy(lambda m: m.id)
+    if (t%10 == 9): motors = motors.sortBy(lambda m: m.id)
 
 motors.persist()
 
@@ -119,7 +119,7 @@ print 'maintenance mode:', motors.first().maint_type
 for t in np.arange(Time_start_pred_maint, Time_stop_pred_maint):
     motors = motors.map(lambda m: m.operate())
     #this inelegant step triggers lazy execution and avoids 'excessively deep recursion' error
-    if (t%300 == 299): motors = motors.sortBy(lambda m: m.id)
+    if (t%10 == 9): motors = motors.sortBy(lambda m: m.id)
 
 motors.persist()
 
