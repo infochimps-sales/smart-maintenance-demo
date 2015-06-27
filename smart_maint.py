e16e6880c3f23ffa2b35e06a62f92107ea9b6fa1
JoeHahnIC <joe.hahn@infochimps.com>
2015-06-27 11:10:13 -0500

    ran nums

--- smart_maint.py
+++ smart_maint.py
@@ -79,8 +79,9 @@ start_time_sec = time.clock()
 maint_type = 'run-to-fail'
 
 #create parallelized list of motors
-num_partitions = 3*7*2    #3datanotes*(7 of 8 vcpus on m3.2xl)*2partitions_per_cpu 163sec
-num_partitions = 3*7*5    #3datanotes*(7 of 8 vcpus on m3.2xl)*5partitions_per_cpu 163sec
+num_partitions = 3*7*1    #3datanotes*(7 of 8 vcpus on m3.2xl)*1partitions_per_cpu XXsec
+#num_partitions = 3*7*2    #3datanotes*(7 of 8 vcpus on m3.2xl)*2partitions_per_cpu 163sec
+#num_partitions = 3*7*5    #3datanotes*(7 of 8 vcpus on m3.2xl)*5partitions_per_cpu 177sec
 motors = sc.parallelize(
     [ Motor(motor_id + 100, Time_start_runtofail, maint_type, fail_prob_rate, 
         Temp_0, delta_Temp, Pressure_0, delta_Pressure, maint_interval, maint_duration, 
