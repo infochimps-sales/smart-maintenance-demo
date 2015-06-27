eb2869a93a15f28540a37f4091f2a74d698a77b7
JoeHahnIC <joe.hahn@infochimps.com>
2015-06-27 11:02:20 -0500

    ran nums

--- smart_maint.py
+++ smart_maint.py
@@ -91,8 +91,8 @@ print 'maintenance mode:', motors.first().maint_type
 for t in np.arange(Time_start_runtofail, Time_stop_runtofail):
     motors = motors.map(lambda m: m.operate())
     ##this inelegant step triggers lazy execution and avoids 'excessively deep recursion' error
-    if (t%50 == 49): 
-        motors = motors.sortBy(lambda m: m.id)
+    #if (t%50 == 49): 
+    #    motors = motors.sortBy(lambda m: m.id)
 
 #run motors using scheduled maintenance
 maint_type = 'scheduled'
