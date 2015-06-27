27b366900592ae393797d34cd43782da0606b6bd
JoeHahnIC <joe.hahn@infochimps.com>
2015-06-26 21:42:04 -0500

    spark

--- smart_maint.py
+++ smart_maint.py
@@ -104,7 +104,7 @@ maint_type = 'scheduled'
 motors = motors.map(lambda m: m.set_maint_type(maint_type))
 print 'maintenance mode:', motors.first().maint_type
 for t in np.arange(Time_start_sched_maint, Time_stop_sched_maint):
-    motors = motors.map(lambda m: m.operate())
+    motors = motors.map(lambda m: m.operate()).persist()
     ##this inelegant step triggers lazy execution and avoids 'excessively deep recursion' error
     if (t%10 == 9): 
         motors = motors.sortBy(lambda m: m.id)
