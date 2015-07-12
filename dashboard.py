28690e2959ec399f1974651d680dc394a73cff20
JoeHahnIC <joe.hahn@infochimps.com>
2015-07-12 11:55:48 -0500

    dash

--- dashboard.py
+++ dashboard.py
@@ -157,47 +157,10 @@ motor_source = ColumnDataSource(
 		maintenance = N.maintenance.tolist(),
 		repair = N.repair.tolist(),
 		total = N.total.tolist(),
-	)
-)
-motor_fig = figure(title='Number of Motors    (click-drag to zoom)', x_axis_label='Time', 
-	y_axis_label='Number of motors', tools='box_select, hover, reset',
-	plot_width=1000, plot_height=300, x_range=[0, 1200], y_range=[-10, 210])
-motor_fig.title_text_font_size = '15pt'
-motor_fig.xaxis.axis_label_text_font_size = '11pt'
-motor_fig.yaxis.axis_label_text_font_size = '11pt'
-motor_fig.circle('Time', 'total', color='blue', source=motor_source, legend='total', alpha=1.0)
-motor_fig.circle('Time', 'operating', color='green', source=motor_source, legend='operating', alpha=1.0)
-motor_fig.circle('Time', 'maintenance', color='orange', source=motor_source, legend='maintenance', alpha=0.75)
-motor_fig.circle('Time', 'repair', color='red', source=motor_source, legend='repair', alpha=1.0)
-motor_fig.legend.orientation = "top_right"
-motor_fig.patch([0, 200, 200, 0], [-10, -10, 210, 210], color='lightsalmon', alpha=0.35, 
-	line_width=0)
-motor_fig.patch([200, 400, 400, 200], [-10, -10, 210, 210], color='gold', alpha=0.35, 
-	line_width=0)
-motor_fig.patch([400, 1200, 1200, 400], [-10, -10, 210, 210], color='darkseagreen', 
-	alpha=0.35, line_width=0)   
-motor_fig.text([ 45], [173], ['run-to-fail'])
-motor_fig.text([245], [173], ['scheduled'])
-motor_fig.text([245], [155], ['maintenance'])
-motor_fig.text([445], [173], ['predictive'])
-motor_fig.text([445], [155], ['maintenance'])
-hover = motor_fig.select(dict(type=HoverTool))
-hover.tooltips = [
-	("       Time", "@Time"),
-	("  operating", "@operating"),
-	("maintenance", "@maintenance"),
-	("     repair", "@repair"),
-	("      total", "@total"),
-]
-
-#plot number of working & broken motors versus time
-motor_source = ColumnDataSource(
-	data=dict(
-		Time = N.Time.tolist(),
-		operating = N.operating.tolist(),
-		maintenance = N.maintenance.tolist(),
-		repair = N.repair.tolist(),
-		total = N.total.tolist(),
+		x = [],
+		y = [],
+		width = [],
+		height = [],
 	)
 )
 motor_fig = figure(title='Number of Motors    (click-drag to zoom)', x_axis_label='Time', 
@@ -229,18 +192,27 @@ hover.tooltips = [
 	("maintenance", "@maintenance"),
 	("     repair", "@repair"),
 	("      total", "@total"),
+	("      x", "@x"),
+	("      y", "@y"),
+	("  width", "@width"),
+	(" height", "@height"),
 ]
-s2 = ColumnDataSource(
-	data=dict(
-		Time = [],
-		operating = [],
-		maintenance = [],
-		repair = [],
-		total = [],
-	)
-)
+jscode="""
+    var data = source.get('data');
+    var start = range.get('start');
+    var end = range.get('end');
+    data['%s'] = [start + (end - start) / 2];
+    data['%s'] = [end - start];
+    source.trigger('change');
+"""
+motor_fig.x_range.callback = Callback(
+    args=dict(source=motor_source, range=motor_fig.x_range), code=jscode % ('x', 'width'))
+motor_fig.y_range.callback = Callback(
+    args=dict(source=motor_source, range=motor_fig.y_range), code=jscode % ('y', 'height'))
+
+s2 = motor_source.clone()
 p2 = figure(title='Number of Motors    (click-drag to zoom)', x_axis_label='Time', 
-	y_axis_label='Number of motors', tools='box_select, hover, reset',
+	y_axis_label='Number of motors', tools='',
 	plot_width=1000, plot_height=300, x_range=[0, 1200], y_range=[-10, 210])
 p2.circle('Time', 'total', color='blue', source=s2, legend='total', alpha=1.0)
 p2.circle('Time', 'operating', color='green', source=s2, legend='operating', alpha=1.0)
@@ -250,21 +222,20 @@ motor_source.callback = Callback(args=dict(s2=s2), code="""
     var inds = cb_obj.get('selected')['1d'].indices;
     var d1 = cb_obj.get('data');
     var d2 = s2.get('data');
-    d2['Time'] = []
-    d2['operating'] = []
-    d2['maintenance'] = []
-    d2['repair'] = []
-    d2['total'] = []
+    d2['Time'] = [];
+    d2['operating'] = [];
+    d2['maintenance'] = [];
+    d2['repair'] = [];
+    d2['total'] = [];
     for (i = 0; i < inds.length; i++) {
-        d2['Time'].push(d1['Time'][inds[i]])
-        d2['operating'].push(d1['operating'][inds[i]])
-        d2['maintenance'].push(d1['maintenance'][inds[i]])
-        d2['repair'].push(d1['repair'][inds[i]])
-        d2['total'].push(d1['total'][inds[i]])
+        d2['Time'].push(d1['Time'][inds[i]]);
+        d2['operating'].push(d1['operating'][inds[i]]);
+        d2['maintenance'].push(d1['maintenance'][inds[i]]);
+        d2['repair'].push(d1['repair'][inds[i]]);
+        d2['total'].push(d1['total'][inds[i]]);
     }
     s2.trigger('change');
 """)
-
 
 #export plot to html and return
 #plot_grid = vplot(dec_fig, earn_fig, rev_fig, motor_fig, vform(N_table))
