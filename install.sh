c82bb994252be3dd2690c063978cf4dec407bf4f
JoeHahnIC <joe.hahn@infochimps.com>
2015-12-07 16:42:08 -0600

    fix pandas error

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ for host in "${hosts[@]}"; do
         #wget -nv http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/Anaconda-2.1.0-Linux-x86_64.sh;
         wget -nv https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda-2.3.0-Linux-x86_64.sh;
         #install anaconda
-        chmod +x Anaconda-2.1.0-Linux-x86_64.sh;
+        chmod +x Anaconda-2.3.0-Linux-x86_64.sh;
         ./Anaconda-2.1.0-Linux-x86_64.sh -b;
         rm -rf Anaconda-2.1.0-Linux-x86_64.sh;
         #upgrade bokeh
