a78509c6bac3f5e60c6fa2310a5a84926d4d6721
JoeHahnIC <joe.hahn@infochimps.com>
2015-12-07 16:38:09 -0600

    fix pandas error

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ for host in "${hosts[@]}"; do
         rm -rf Anaconda-2.1.0-Linux-x86_64.sh* anaconda;
         #download anaconda
         #wget -nv http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/Anaconda-2.1.0-Linux-x86_64.sh;
-        wget -nv wget -nv https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda-2.3.0-Linux-x86_64.sh;
+        wget -nv https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda-2.3.0-Linux-x86_64.sh;
         #install anaconda
         chmod +x Anaconda-2.1.0-Linux-x86_64.sh;
         ./Anaconda-2.1.0-Linux-x86_64.sh -b;
