8a5d181b835de8f09f5de9c4f95abea7de6fb86a
JoeHahnIC <joe.hahn@infochimps.com>
2016-06-22 10:44:21 -0500

    smart_maint.py

--- smart_maint.py
+++ smart_maint.py
@@ -97,16 +97,16 @@ for t in np.arange(Time_start_runtofail, Time_stop_runtofail):
 
 #run motors using scheduled maintenance
 maint_type = 'scheduled'
+import sys
+sys.exit()
 motors = motors.map(lambda m: m.set_maint_type(maint_type))
+import sys
+sys.exit()
 print 'maintenance mode:', motors.first().maint_type
 for t in np.arange(Time_start_sched_maint, Time_stop_sched_maint):
     motors = motors.map(lambda m: m.operate())
     #inelegant way to trigger lazy execution and avoid 'excessively deep recursion' error
-    #if (t%10 == 9): motors = motors.sortBy(lambda m: m.id)
-    print t
-
-import sys
-sys.exit()
+    if (t%100 == 99): motors = motors.sortBy(lambda m: m.id)
 
 #train SVM to do predictive maintenance 
 motors_local = motors.collect()
