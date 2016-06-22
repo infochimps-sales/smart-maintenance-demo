f1f90f8182204744d998ea73604b16d5a3d42d80
JoeHahnIC <joe.hahn@infochimps.com>
2016-06-22 10:36:28 -0500

    smart_maint.py

--- smart_maint.py
+++ smart_maint.py
@@ -102,7 +102,7 @@ print 'maintenance mode:', motors.first().maint_type
 for t in np.arange(Time_start_sched_maint, Time_stop_sched_maint):
     motors = motors.map(lambda m: m.operate())
     #inelegant way to trigger lazy execution and avoid 'excessively deep recursion' error
-    if (t%100 == 99): motors = motors.sortBy(lambda m: m.id)
+    if (t%10 == 9): motors = motors.sortBy(lambda m: m.id)
 
 import sys
 sys.exit()
