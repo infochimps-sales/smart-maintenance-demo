eeca86898ee1ae3b0f8d70c9f9d9bca1a6a751df
JoeHahnIC <joe.hahn@infochimps.com>
2016-06-22 10:33:06 -0500

    smart_maint.py

--- smart_maint.py
+++ smart_maint.py
@@ -90,13 +90,13 @@ motors = sc.parallelize(
         repair_duration, pred_maint_buffer_Time, training_axes, prediction_axis)
     for motor_id in np.arange(N_motors) ], numSlices=num_partitions )
 
-import sys
-sys.exit()
-
 #run motors using run-to-fail maintenance 
 print 'maintenance mode:', motors.first().maint_type
 for t in np.arange(Time_start_runtofail, Time_stop_runtofail):
     motors = motors.map(lambda m: m.operate())
+
+import sys
+sys.exit()
 
 #run motors using scheduled maintenance
 maint_type = 'scheduled'
